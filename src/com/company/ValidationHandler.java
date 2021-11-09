399423112c9a8a185a1c111fea0a6e51cec669d1
gustininho <gustas.dersonas@gmail.com>
2021-11-09 21:16:49 +0000

    add multiple error showing

--- src/com/company/ValidationHandler.java
+++ src/com/company/ValidationHandler.java
@@ -7,22 +7,30 @@ public class ValidationHandler {
     static PhoneValidator numVal = new PhoneValidator();
 
     static Boolean isUserInfoValid(User user) {
+        boolean isGood = true;
+
         if (passVal.isValid(user.password)==false) {
             System.out.println();
             System.out.println("Ivestas slaptazodis nera validus");
-            return false;
+            isGood = false;
         }
         if (emailVal.isValid(user.email)==false) {
             System.out.println();
             System.out.println("Ivestas el. pastas nera validus");
-            return false;
+            isGood = false;
         }
         if (numVal.isValid(user.number)==false) {
             System.out.println();
             System.out.println("Ivestas telefono numeris nera validus");
+            isGood = false;
+        }
+
+        if (isGood==true) {
+            return true;
+        }
+        else {
             return false;
         }
-        return true;
     }
 
     static Boolean isEmailValid(String email) {
